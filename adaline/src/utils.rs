8a04c2ab3b50b3ddfa9dd1aae82475ef1c149499
Jorge Hevia <jorge.hevia@outlook.com>
2019-04-04 19:54:01 +0200

    Fixing the mofify_weights _> always increasing weights

--- adaline/src/utils.rs
+++ adaline/src/utils.rs
@@ -67,33 +67,47 @@ impl Adaline {
         }
     }
 
-    pub fn calculate_output(&self, data: &Data) -> f64 {
+    pub fn calculate_output_per_row(&self, data: &Data) -> f64 {
         assert_eq!(data.inputs.len(), self.weights.len());
         let mut output = 0.0;
 
-        for input in &data.inputs {
-            for weight in &self.weights {
-                output += (input - weight).powi(2) + self.threshold;
-            }
+        for _i in 0..data.inputs.len() {
+            output += data.inputs[_i] * self.weights[_i];
         }
 
-        return output;
+        return output + self.threshold;
     }
-
+/*
+def modifyWeights(output, row, weights,  threshold, learnfactor):
+    weightsTemp = 0.0
+    learnedDiff = learnfactor * (desired_output - output)
+    for x in xrange(0, len(weights)):
+        weightsTemp = learnedDiff * row[x] # learnedDiff is calculated outside the loop for better performance
+        weights[x] = weights[x] + weightsTemp
+    threshold = threshold + learnedDiff
+    return weights, threshold
+ */
     pub fn modify_weights(&mut self, data: &Data) {
         let learned_difference = self.learnfactor * (data.output - self.output);
         self.threshold += learned_difference;
         for i in 0..self.weights.len() {
-            self.weights[i] += &learned_difference * &data.inputs[i]
+            self.weights[i] += learned_difference * data.inputs[i]
         }
     }
 
     pub fn calculate_error(&mut self, data_rows: &Vec<Data>) -> f64 {
         let mut error: f64 = 0.0;
         for data in data_rows {
-            let output: f64 = self.calculate_output(&data);
+            let output: f64 = self.calculate_output_per_row(&data);
             error += (data.output - &output).powi(2);
             self.modify_weights(&data);
+            
+            println!(">>>");
+            println!("data.output: {:?}", data.output);
+            println!("output: {:?}", output);
+            println!("error: {:?}", error);
+            println!("weights: {:?}", self.weights[0]);
+            println!("<<<");
         }
         return error / data_rows.len() as f64;
     }
