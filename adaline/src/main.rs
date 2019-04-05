acbe6cd1b5f85114b90430f647417970d72693ef
Jorge Hevia <jorge.hevia@outlook.com>
2019-04-05 17:10:11 +0200

    Halfway fixing the training function and added tests.

--- adaline/src/main.rs
+++ adaline/src/main.rs
@@ -13,40 +13,8 @@ fn main() {
 
     let data: Vec<Data> = csv_reader.read_data_from_csv();
 
-    let error = adaline.calculate_error(&data);
-    println!("Error calculated: {:?}", error);
-}
-
-#[test]
-fn test_modify_weights() {
-    let mut adaline = Adaline::new(8, 0.05);
-    let mut csv_reader = CsvReader::new("src/RandomizedDataTraining.csv");
-    &csv_reader.read_content();
-    let data_list: Vec<Data> = csv_reader.read_data_from_csv();
-    println!("unmodified weights: {:?}", &adaline.weights);
-    adaline.modify_weights(&data_list[0]);
-    println!("weights: {:?}", &adaline.weights);
-}
-#[test]
-fn full_test() {
-    // setup
-    let mut adaline = Adaline::new(8, 0.05);
-    let mut csv_reader = CsvReader::new("src/RandomizedDataTraining.csv");
-    &csv_reader.read_content();
-    let data_list: Vec<Data> = csv_reader.read_data_from_csv();
-    
-    // check the weights
-    //adaline.modify_weights(&data_list[0]);
-    
-    // run!
-    let mut iter: i32 = 1000;
+    let iter = 10;
     println!("Running training for {} interations", iter);
-    let error: f64 = adaline.training(iter, &data_list);
+    let error: [Vec<f64>;2] = adaline.cycle(&data, iter);
     println!("Final error: {:?}", &error);
- 
-    iter = 10000;
-    println!("Running training for {} interations", iter);
-    let error: f64 = adaline.training(iter, &data_list);
-    println!("Final error: {:?}", &error);
-    
 }
--- adaline/src/utils.rs
+++ adaline/src/utils.rs
@@ -7,6 +7,10 @@ use std::fs::File;
 use std::io::Read;
 use std::string::String;
 
+use std::error::Error;
+use std::io::prelude::*;
+use std::path::Path;
+
 #[derive(Debug)]
 pub struct CsvReader {
     file_name: String,
@@ -27,8 +31,8 @@ impl CsvReader {
 
     pub fn read_content(&mut self) {
         self.file
-            .read_to_string(&mut self.raw_content)
-            .expect("Error reading file content.");
+        .read_to_string(&mut self.raw_content)
+        .expect("Error reading file content.");
     }
 
     pub fn read_data_from_csv(&mut self) -> Vec<Data> {
@@ -36,9 +40,9 @@ impl CsvReader {
 
         for line in self.raw_content.lines() {
             let mut inputs = line
-                .split(',')
-                .filter_map(|s| s.parse::<f64>().ok())
-                .collect::<Vec<_>>();
+            .split(',')
+            .filter_map(|s| s.parse::<f64>().ok())
+            .collect::<Vec<_>>();
             let output: f64 = inputs.pop().expect("Ops");
             data.push(Data {
                 inputs: inputs,
@@ -57,7 +61,9 @@ pub struct Adaline {
     learnfactor: f64
 }
 impl Adaline {
-    /// Initializes the Adaline struct
+    /*
+        Public functions, only need / use these
+     */
     pub fn new(inputs: u8, learnfactor: f64) -> Adaline {
         Adaline {
             weights: random_floats64_vector(inputs),
@@ -77,17 +83,25 @@ impl Adaline {
 
         return output + self.threshold;
     }
-/*
-def modifyWeights(output, row, weights,  threshold, learnfactor):
-    weightsTemp = 0.0
-    learnedDiff = learnfactor * (desired_output - output)
-    for x in xrange(0, len(weights)):
-        weightsTemp = learnedDiff * row[x] # learnedDiff is calculated outside the loop for better performance
-        weights[x] = weights[x] + weightsTemp
-    threshold = threshold + learnedDiff
-    return weights, threshold
- */
-    pub fn modify_weights(&mut self, data: &Data) {
+
+    pub fn cycle(&mut self, data_rows: &Vec<Data>, cycles: i32) -> [Vec<f64>;2]{
+        let mut error_training: Vec<f64> = Vec::new();
+        let mut error_validation: Vec<f64> = Vec::new();
+
+        for _i in 0..cycles {
+            self.training(data_rows);
+            &error_training.push(self.calculate_error(data_rows));
+            &error_validation.push(self.calculate_error(data_rows));
+        }
+
+        return [error_training, error_validation];
+    }
+
+    /*
+        Private functions. Do NOT atemp to use this on your own.
+     */
+
+    fn modify_weights(&mut self, data: &Data) {
         let learned_difference = self.learnfactor * (data.output - self.output);
         self.threshold += learned_difference;
         for i in 0..self.weights.len() {
@@ -95,45 +109,45 @@ def modifyWeights(output, row, weights,  threshold, learnfactor):
         }
     }
 
-    pub fn calculate_error(&mut self, data_rows: &Vec<Data>) -> f64 {
+    fn calculate_error(&mut self, data_rows: &Vec<Data>) -> f64 {
         let mut error: f64 = 0.0;
         for data in data_rows {
             let output: f64 = self.calculate_output_per_row(&data);
             error += (data.output - &output).powi(2);
             self.modify_weights(&data);
-            
-            println!(">>>");
-            println!("data.output: {:?}", data.output);
-            println!("output: {:?}", output);
-            println!("error: {:?}", error);
-            println!("weights: {:?}", self.weights[0]);
-            println!("<<<");
         }
         return error / data_rows.len() as f64;
     }
 
-    /*
-    '''
-    Name: training
-    Function: makes one cylce of training (from 3 to 5)
-    Input: rows, weights, threshold, learnfactor 
-    Returns: weights, threshold after the training
-'''
-def training(rows, weights, threshold, learnfactor):
-    for x in rows:
-        output = calculateOutputPerRow(x[0:len(weights)], weights, threshold)
-        weights, threshold = modifyWeights(output, x, weights, threshold, learnfactor)
-    return weights, threshold
-    */
-    pub fn training(&mut self, cycles: i32, data_rows: &Vec<Data>) -> f64 {
-        let mut lowest_error: f64 = 10000.0;
-        for _i in 0..cycles {
-            let current_error: f64 = self.calculate_error(&data_rows);
-            if current_error < lowest_error {
-                lowest_error = current_error;
-            }
+
+    fn training(&mut self, data_rows: &Vec<Data>) {
+        for data in data_rows {
+            self.output = self.calculate_output_per_row(data);
+            self.modify_weights(data);
         }
-        lowest_error
+    }
+
+    fn write_errors_in_csv(self, errors: [Vec<f64>;2]) {
+        let training = Path::new("error_training");
+
+        // Open a file in write-only mode, returns `io::Result<File>`
+        let mut training_file = match File::create(&training) {
+            Err(why) => panic!("couldn't create error_training: {}",
+               why.description()),
+            Ok(file) => file,
+        };
+
+        for i in 0..errors[0].len() {
+            let line = &format!("{},{}\n", errors[0][i],errors[1][i]);
+            // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+            match training_file.write_all(line.as_bytes()) {
+                Err(why) => {
+                    panic!("couldn't write to training_file: {}",
+                     why.description())
+                },
+                Ok(_) => (),
+            }
+        }        
     }
 }
 
@@ -154,4 +168,58 @@ pub fn random_floats64_vector(lenght: u8) -> Vec<f64> {
 pub fn random_float64() -> f64 {
     let mut rng = thread_rng();
     return rng.gen_range(-1.0, 1.0);
+}
+
+#[test]
+fn test_random_float() {
+    let rnd: f64 = random_float64();
+    assert!(rnd <= 1.0, "weight should be less or equal than 1.0");
+    assert!(rnd >= -1.0, "weight should be greater or equal than -1.0");
+}
+#[test]
+fn test_random_floats_vector() {
+    let rnd_floats64: Vec<f64> = random_floats64_vector(8);
+    for rnd in rnd_floats64 {
+        assert!(rnd <= 1.0, "weight should be less or equal than 1.0");
+        assert!(rnd >= -1.0, "weight should be greater or equal than -1.0");
+    }
+}
+#[test]
+fn test_modify_weights() {
+    let mut adaline = Adaline::new(8, 0.05);
+    let mut csv_reader = CsvReader::new("src/RandomizedDataTraining.csv");
+    &csv_reader.read_content();
+    let data_list: Vec<Data> = csv_reader.read_data_from_csv();
+    println!("unmodified weights: {:?}", &adaline.weights);
+    adaline.modify_weights(&data_list[0]);
+    println!("weights: {:?}", &adaline.weights);
+}
+#[test]
+fn test_calculate_output() {
+    // setup
+    let adaline = Adaline::new(8, 0.05);
+    let mut csv_reader = CsvReader::new("src/RandomizedDataTraining.csv");
+    &csv_reader.read_content();
+    let data_list: Vec<Data> = csv_reader.read_data_from_csv();
+
+    let output: f64 = adaline.calculate_output_per_row(&data_list[0]);
+    assert!(output <= 1.0, "calculated output should be less or equal than 1.0");
+    assert!(output >= -1.0, "calculated output should be greater or equal than -1.0");
+
+}
+#[test]
+fn full_test() {
+    // setup
+    let mut adaline = Adaline::new(8, 0.05);
+    let mut csv_reader = CsvReader::new("src/RandomizedDataTraining.csv");
+    &csv_reader.read_content();
+    let data_list: Vec<Data> = csv_reader.read_data_from_csv();
+
+    // run!
+    let iter = 1000;
+    println!("Running training for {} interations", iter);
+    let error: [Vec<f64>;2] = adaline.cycle(&data_list, iter);
+    assert!(error[0].len() > 1, "error_training should contain more than one value");
+    assert!(error[1].len() > 1, "error_validation should contain more than one value");
+    adaline.write_errors_in_csv(error);    
 }
